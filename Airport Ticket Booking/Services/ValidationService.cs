92fcf8605505044f4247987d2ce008cf5b279080
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-10 16:51:44 +0200

    Add validation methods to validate User properties to ValidationService class.

--- Airport Ticket Booking/Services/ValidationService.cs
+++ Airport Ticket Booking/Services/ValidationService.cs
@@ -1,8 +1,32 @@
 ﻿using System.Text.RegularExpressions;
-using Airport_Ticket_Booking.Models;
 
 namespace Airport_Ticket_Booking.Services;
 
 public class ValidationService
 {
+    public static bool IsValidFullName(string fullName)
+    {
+        return
+            !string.IsNullOrEmpty(fullName) &&
+            fullName.Length <= 100;
+    }
+
+    public static bool IsValidEmail(string email)
+    {
+        string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        return Regex.IsMatch(email, emailPattern);
+    }
+
+    public static bool IsValidPhoneNumber(string phoneNumber)
+    {
+        string phonePattern = @"^\+\d{10}$";
+
+        return Regex.IsMatch(phoneNumber, phonePattern);
+    }
+
+    public static bool IsValidDateOfBirth(DateTime dateOfBirth)
+    {
+        return dateOfBirth <= DateTime.Now;
+    }
 }
