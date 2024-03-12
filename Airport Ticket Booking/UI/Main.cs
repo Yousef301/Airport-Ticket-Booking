a330c6b1d09d54d92b6de3464614073f03f24346
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-12 14:55:03 +0200

    Apply functionality for searching feature.

--- Airport Ticket Booking/UI/Main.cs
+++ Airport Ticket Booking/UI/Main.cs
@@ -100,23 +100,112 @@ public class Main
 
                                 break;
                             case "2":
+                                Console.Clear();
+                                var selection1 = 0;
+                                Dictionary<string, object> filter = new Dictionary<string, object>();
+
+                                do
+                                {
+                                    Menus.SearchForFlightMenu();
+                                    int.TryParse(Console.ReadLine(), out selection1);
+
+                                    switch (selection1)
+                                    {
+                                        case 1:
+                                            var price = EnterValue("Price");
+
+                                            if (double.TryParse(price, out double pri))
+                                            {
+                                                if (!filter.ContainsKey("Price"))
+                                                {
+                                                    filter.Add("Price", pri);
+                                                }
+                                                else
+                                                {
+                                                    filter["Price"] = pri;
+                                                }
+                                            }
+
+                                            break;
+                                        case 2:
+                                            UpdateFilter("DepartureCountry", "Departure Country", filter);
+                                            break;
+                                        case 3:
+                                            UpdateFilter("DestinationCountry", "Destination Country", filter);
+                                            break;
+                                        case 4:
+                                            var departureDate = EnterValue("Departure Date (yyyy-MM-dd)");
+
+                                            if (DateTime.TryParseExact(departureDate, "yyyy-MM-dd", null,
+                                                    System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
+                                            {
+                                                if (!filter.ContainsKey("DepartureDate"))
+                                                {
+                                                    filter.Add("DepartureDate", parsedDate);
+                                                }
+                                                else
+                                                {
+                                                    filter["DepartureDate"] = parsedDate;
+                                                }
+                                            }
+
+                                            break;
+                                        case 5:
+                                            UpdateFilter("DepartureAirport", "Departure Airport", filter);
+                                            break;
+                                        case 6:
+                                            UpdateFilter("ArrivalAirport", "Arrival Airport", filter);
+                                            break;
+                                        case 7:
+                                            var flightClass = EnterValue("Flight Class");
+
+                                            if (Enum.TryParse(flightClass, true, out FlightClass flightClassValue))
+                                            {
+                                                if (!filter.ContainsKey("FlightClass"))
+                                                {
+                                                    filter.Add("FlightClass", flightClassValue);
+                                                }
+                                                else
+                                                {
+                                                    filter["FlightClass"] = flightClassValue;
+                                                }
+                                            }
+
+                                            break;
+                                        case 8:
+                                            ViewFilteredFlights(filter);
+                                            break;
+                                        case 9:
+                                            break;
+                                        default:
+                                            Log.InvalidInputMessage(
+                                                "Invalid input. Please enter a number between 1 and 9.");
+                                            break;
+                                    }
+
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.Write("Enter your selection: ");
+                                    Console.ResetColor();
+                                } while (selection1 != 9);
+
+
                                 break;
                             case "3":
                                 Console.Clear();
 
-                                var selection1 = "0";
+                                var selection2 = "0";
 
                                 do
                                 {
                                     Menus.ManageBookingsMenu();
-                                    selection1 = Console.ReadLine();
+                                    selection2 = Console.ReadLine();
 
-                                    if (selection1 == "" || !int.TryParse(selection1, out _))
+                                    if (selection2 == "" || !int.TryParse(selection2, out _))
                                     {
-                                        selection1 = "0";
+                                        selection2 = "0";
                                     }
 
-                                    switch (selection1)
+                                    switch (selection2)
                                     {
                                         case "1":
                                             Console.Clear();
@@ -158,7 +247,7 @@ public class Main
                                     Console.ForegroundColor = ConsoleColor.Cyan;
                                     Console.Write("Enter your selection: ");
                                     Console.ResetColor();
-                                } while (Int32.Parse(selection1) != 3);
+                                } while (Int32.Parse(selection2) != 3);
 
                                 break;
                             case "4":
@@ -189,5 +278,33 @@ public class Main
             Console.Write("Please enter you id to login: ");
             Console.ResetColor();
         } while (!valid);
+    }
+
+    static void UpdateFilter(string key, string prompt, Dictionary<string, object> filter)
+    {
+        var value = EnterValue(prompt);
+        if (!string.IsNullOrEmpty(value))
+        {
+            if (!filter.ContainsKey(key))
+            {
+                filter.Add(key, value);
+            }
+            else
+            {
+                filter[key] = value;
+            }
+        }
+    }
+
+    static void ViewFilteredFlights(Dictionary<string, object> filter)
+    {
+        List<Flight> flights = FlightService.SearchForFlight(filter);
+        Flight.ViewFlights(flights);
+    }
+
+    static string EnterValue(string prompt)
+    {
+        Console.Write($"Enter {prompt}: ");
+        return Console.ReadLine();
     }
 }
