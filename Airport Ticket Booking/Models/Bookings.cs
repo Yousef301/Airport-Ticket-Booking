5a270771e3871db02f733b504733bb5ad04f5edd
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-12 10:50:07 +0200

    Add Bookings model.

---
+++ Airport Ticket Booking/Models/Bookings.cs
@@ -0,0 +1,18 @@
+﻿namespace Airport_Ticket_Booking.Models;
+
+public class Bookings
+{
+    public Passenger Passenger { get; set; }
+    public Flight Flight { get; set; }
+
+    public Bookings(Passenger passenger, Flight flight)
+    {
+        Passenger = passenger;
+        Flight = flight;
+    }
+
+    public override string ToString()
+    {
+        return $"Passenger name -> {Passenger.FullName}\nFlight Details:\n{Flight}";
+    }
+}
