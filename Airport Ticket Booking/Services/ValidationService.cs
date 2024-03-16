cab9a669b77002f935b03a0fafab99c6d3ab1eac
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-16 13:57:48 +0200

    Update ValidateObject method.

--- Airport Ticket Booking/Services/ValidationService.cs
+++ Airport Ticket Booking/Services/ValidationService.cs
@@ -1,46 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
+﻿using System.ComponentModel.DataAnnotations;
 using Airport_Ticket_Booking.Models;
 
-namespace Airport_Ticket_Booking.Services
+namespace Airport_Ticket_Booking.Services;
+
+public class ValidationService
 {
-    public class ValidationService
+    public static bool ValidateObject<T>(T obj)
     {
-        public static bool ValidateObject<T>(T obj)
+        var validationContext = new ValidationContext(obj);
+        var validationResults = new List<ValidationResult>();
+
+        bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+        if (!isValid)
         {
-            var validationContext = new ValidationContext(obj);
-            var validationResults = new List<ValidationResult>();
-
-            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
-
-            if (!isValid)
+            using StreamWriter writer =
+                new StreamWriter(
+                    "C:\\Users\\shama\\RiderProjects\\Airport Ticket Booking\\Airport Ticket Booking\\Logs\\LoadedFlightsLog.txt",
+                    true);
+            if (typeof(T) == typeof(Flight))
             {
-                using StreamWriter writer =
-                    new StreamWriter(
-                        "C:\\Users\\shama\\RiderProjects\\Airport Ticket Booking\\Airport Ticket Booking\\Logs\\LoadedFlightsLog.txt",
-                        true);
-                if (typeof(T) == typeof(Flight))
-                {
-                    Flight flight = obj as Flight;
-                    writer.WriteLine(
-                        $"{DateTime.Now}: {typeof(T).Name} {flight.FlightId} is invalid. Validation errors:");
-                }
-                else
-                {
-                    writer.WriteLine($"{DateTime.Now}: {typeof(T).Name} is invalid. Validation errors:");
-                }
-
-                foreach (var validationResult in validationResults)
-                {
-                    writer.WriteLine($"- {validationResult.ErrorMessage}");
-                }
-
-                writer.WriteLine();
+                Flight flight = obj as Flight;
+                writer.WriteLine(
+                    $"{DateTime.Now}: {typeof(T).Name} {flight.FlightId} is invalid. Validation errors:");
+            }
+            else
+            {
+                writer.WriteLine($"{DateTime.Now}: {typeof(T).Name} is invalid. Validation errors:");
             }
 
-            return isValid;
+            foreach (var validationResult in validationResults)
+            {
+                writer.WriteLine($"- {validationResult.ErrorMessage}");
+            }
+
+            writer.WriteLine();
         }
+
+        return isValid;
+    }
+
+    public static bool IsValidValue<T>(T value, T min, T max) where T : IComparable<T>
+    {
+        return min.CompareTo(value) <= 0 && value.CompareTo(max) <= 0;
     }
 }
