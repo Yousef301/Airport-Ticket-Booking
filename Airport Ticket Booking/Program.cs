238818df7e44bc3c9c59947a4dea65d3c0bcc0a8
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-16 14:04:27 +0200

    Run console project.

--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -1,10 +1,9 @@
-﻿using Airport_Ticket_Booking.Models;
-
-namespace Airport_Ticket_Booking;
+﻿namespace Airport_Ticket_Booking;
 
 public class Airport
 {
     static void Main(string[] args)
     {
+        UI.Main.Run();
     }
 }
