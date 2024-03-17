a8e7b497d3404c6863f289ecf728918ddd1ba6d3
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-03-17 14:14:06 +0200

    Use AppSettings.json instead of App.config
    Using AppSettings.json instead of App.config as configuration file because the support of .NET8. Also a class called CustomConfigurationManager was implemented to handle the open of AppSettings file so it's give me the functionality to read data from the file.

---
+++ Airport Ticket Booking/Helpers/CustomConfigurationManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Airport_Ticket_Booking.Helpers;
+
+public class CustomConfigurationManager
+{
+    private readonly IConfiguration _configuration;
+
+    public CustomConfigurationManager(string configFile)
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(configFile, optional: true, reloadOnChange: true);
+
+        _configuration = builder.Build();
+    }
+
+    public string GetValue(string key)
+    {
+        return _configuration[key];
+    }
+}
